5c10bd2523db892b8c544d3714ca71ec812ddc5b
saurabhlively-barclays <saurabhlively-barclays>
2023-11-16 15:36:51 +0530

    Change greeting and libraries to download.

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
+import os
+import pymongo
+import json
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Saurabh!"
 
 if __name__ == "__main__":
     application.run()
